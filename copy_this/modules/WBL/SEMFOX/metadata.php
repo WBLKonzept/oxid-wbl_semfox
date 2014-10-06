1e0c990b0b4acd5f4c8d6bea553be208433ab85a
WBL_b3nl <b.lange@wbl-konzept.de>
2014-10-06 22:24:50 +0200

    - dev notice

--- copy_this/modules/WBL/SEMFOX/metadata.php
+++ copy_this/modules/WBL/SEMFOX/metadata.php
@@ -119,5 +119,5 @@
         'title'       => 'WBL SEMFOX',
         'thumbnail'   => 'wbl_logo.jpg',
         'url'         => 'http://wbl-konzept.de',
-        'version'     => '1.0.0-dev'
+        'version'     => '1.0.0'
     );
